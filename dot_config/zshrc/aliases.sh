efff39f5b166aca07f58988e3f09bcc556dbacce
Shaked Lokits <shaked.lokits@gmail.com>
2025-11-23 12:00:15 +0000

    Split zsh configurations.

---
+++ dot_config/zshrc/aliases.sh
@@ -0,0 +1,9 @@
+#                                           
+#     ▄▄▄  ▄▄    ▄▄  ▄▄▄   ▄▄▄▄ ▄▄▄▄▄  ▄▄▄▄ 
+#    ██▀██ ██    ██ ██▀██ ███▄▄ ██▄▄  ███▄▄ 
+#    ██▀██ ██▄▄▄ ██ ██▀██ ▄▄██▀ ██▄▄▄ ▄▄██▀ 
+#                                          
+#    This file is in charge of aliases.
+
+alias ls="eza -alhUGg --git"
+alias cat=bat 
---
+++ dot_config/zshrc/completion.sh
@@ -0,0 +1,13 @@
+#                                                                 
+#     ▄▄▄▄  ▄▄▄  ▄▄   ▄▄ ▄▄▄▄  ▄▄    ▄▄▄▄▄ ▄▄▄▄▄▄ ▄▄  ▄▄▄  ▄▄  ▄▄ 
+#    ██▀▀▀ ██▀██ ██▀▄▀██ ██▄█▀ ██    ██▄▄    ██   ██ ██▀██ ███▄██ 
+#    ▀████ ▀███▀ ██   ██ ██    ██▄▄▄ ██▄▄▄   ██   ██ ▀███▀ ██ ▀██ 
+#                                                                 
+#    This file is in charge of prompt and text completion.
+#    All of the required completion scripts should be here.
+
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light changyuheng/zsh-interactive-cd
+zinit light b4b4r07/enhancd
+zinit light "MichaelAquilina/zsh-you-should-use"
+ 
